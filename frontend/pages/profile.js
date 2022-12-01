6f96bbf35d2ed7710c689a51eb018efb81a92ad6
Roy Feng <roy.feiyang.feng@gmail.com>
2022-11-30 22:55:08 -0800

    added sign out functionality

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,16 +1,24 @@
-import { Auth, Typography, Button, ThemeSupa } from '@supabase/auth-ui-react'
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { Auth, Button, ThemeSupa } from '@supabase/auth-ui-react'
+import { useSession, useSupabaseClient, useUser } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 
 const Profile = () => {
+  /*
   const session = useSession()
   const supabase = useSupabaseClient()
+  */
+  const supabaseClient = useSupabaseClient()
+  const user = useUser()
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? (
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="light" />
+    <div className="container">
+      {!user ? (
+        <Auth supabaseClient={supabaseClient} providers={['google', 'spotify']} appearance={{ theme: ThemeSupa }} theme="light" />
       ) : (
-        <p>Account page will go here.</p>
+        <>
+          <p>Account page will go here.</p>
+          <button onClick={ () => supabaseClient.auth.signOut() }>Sign out</button>
+        </>
       )}
     </div>
   )
