614a3daaaf3c3fc772caf657a74822cd6d81fbd3
ashley-kim22 <ashkim316@gmail.com>
2022-12-02 03:26:24 -0800

    change thumbnail styling to match designs and use mantine/core

--- frontend/components/thumbnail/thumbnail.js
+++ frontend/components/thumbnail/thumbnail.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import tn_styles from './thumbnail.module.css'
-import { Row, Col } from 'react-bootstrap'
-import Link from 'next/link'
+import React from "react";
+import tn_styles from "./thumbnail.module.css";
+import Link from "next/link";
+import { Avatar, Text, Flex, Space, Image} from "@mantine/core";
+import { rectifyFormat } from "../../utils/formatUTC";
 
-
-const Thumbnail = ({id, name, artist, views, thumbnail}) => {
+const Thumbnail = ({ id, title, username, views, thumbnail, avatar_url, date }) => {
   return (
-    <div className={tn_styles.thumbnail}>
-    <Link target='_blank' href={`/karaoke?vid=${id}`}>
-        <div className={tn_styles.info}>
-            <img src={thumbnail} className={tn_styles.vid_img} />
-              <Row>
-                <Col>
-                  <div className = {tn_styles.title}>{name}</div>
-                  <div>{artist}</div>
-                  <div>{views}</div>
-                </Col>
-              </Row>
-        </div>
-    </Link>
-</div>
-  )
-}
+    <Flex className={tn_styles.thumbnail}>
+        <Flex direction="column">
+        <Link target="_blank" href={`/karaoke?vid=${id}`}>
+          <Image src={thumbnail} layout="fill" radius="md"/>
+        </Link>
+        <Space h={8}/>
+          <Flex sx={{paddingRight: "1rem"}} direction="row">
+            {avatar_url !== undefined ? (
+              <Avatar
+                src={avatar_url}
+                radius="xl"
+                size="sm"
+                alt="no image here"
+              />
+            ) : (
+              <Avatar radius="xl" size="xs" alt="no image here" />
+            )}
+            <Space w={8}/>
+            <Flex direction="column">
+              <Text fz="sm" fw={500}>
+                {title}
+              </Text>
+              <Text fz="xs">{username}</Text>
+              <Flex direction="row">
+                <Text fz="xs">{`${views} views`}</Text>
+                <Space w={8}/>
+                <Text fz="xs">{`${rectifyFormat(date)}`}</Text>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+    </Flex>
+  );
+};
 
-export default Thumbnail
+export default Thumbnail;
