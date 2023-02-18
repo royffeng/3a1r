7164181644dd24a0a611329dc2e4e2e4b7f8a251
royffeng <roy.feiyang.feng@gmail.com>
2023-02-17 17:49:21 -0800

    add login/signup frontend

--- components/Login.js
+++ components/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import LoginLogo from "./LoginLogo";
 import { FaGoogle, FaSpotify } from "react-icons/fa";
@@ -6,6 +6,8 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const Login = () => {
   const supabase = useSupabaseClient();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async (provider) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
@@ -15,6 +17,17 @@ const Login = () => {
 
     if (error) {
       alert("Error Signing in with " + provider);
+    }
+  };
+
+  const handlePasswordLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      alert("Error Signing in with email and password");
     }
   };
 
@@ -31,7 +44,10 @@ const Login = () => {
           <p className="m-0 text-6xl mb-2">Welcome to</p>
           <p className="text-8xl m-0 font-bold">micDrop</p>
         </Col>
-        <Col xl={6} className="flex justify-center items-center flex-col">
+        <Col
+          xl={6}
+          className="flex justify-center items-center flex-col m-0 p-0"
+        >
           <p className="font-bold text-5xl text-left w-1/2">Log In</p>
           <div className="flex flex-col w-1/2">
             <label htmlFor="email" className="m-0.5 text-xl">
@@ -55,25 +71,35 @@ const Login = () => {
               className="rounded-full border-2 border-black px-3 py-2.5 font-semibold text-xl outline-none"
             />
           </div>
+          <div className="w-1/2 flex justify-center items-center">
+            <button className="px-8 py-2 rounded-full border-2 border-black hover:bg-black hover:text-white text-xl mt-8 w-1/2 mr-2">
+              signup
+            </button>
+            <button className="px-8 py-2 rounded-full border-2 border-black hover:bg-black hover:text-white text-xl mt-8 w-1/2 ml-2">
+              login
+            </button>
+          </div>
           <div className="w-1/2 mt-4">
             <p className="text-xl text-center">
               or sign in with a social account
             </p>
-            <div className="flex justify-evenly items-center w-full">
-              <button
-                className="border-2 border-black px-8 py-2 bg-white rounded-full flex hover:!bg-black hover:text-white"
-                onClick={() => handleLogin("google")}
-              >
-                <FaGoogle className="mr-2" />
-                Google
-              </button>
-              <button
-                className="border-2 border-black px-8 py-2 bg-white rounded-full flex hover:!bg-black hover:text-white"
-                onClick={() => handleLogin("spotify")}
-              >
-                <FaSpotify className="mr-2" />
-                Spotify
-              </button>
+            <div className="flex justify-center items-center w-full flex-col">
+              <div className="flex justify-center items-center w-full">
+                <button
+                  className="border-2 border-black px-8 py-2 bg-white rounded-full flex justify-center items-center hover:!bg-black hover:text-white text-xl w-1/2 mr-2"
+                  onClick={() => handleLogin("google")}
+                >
+                  <FaGoogle className="mr-2 text-xl" />
+                  Google
+                </button>
+                <button
+                  className="border-2 border-black px-8 py-2 bg-white rounded-full flex justify-center items-center hover:!bg-black hover:text-white text-xl w-1/2 ml-2"
+                  onClick={() => handleLogin("spotify")}
+                >
+                  <FaSpotify className="mr-2 text-xl" />
+                  Spotify
+                </button>
+              </div>
             </div>
           </div>
         </Col>
