dadc63b685fe75e5ec30ea69cb3fea253de0fbfd
Alamode89 <andreamante89@gmail.com>
2023-02-23 18:47:04 -0800

    setup password view

--- components/Signup/Input.js
+++ components/Signup/Input.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import {RxEyeClosed, RxEyeNone} from "react-icons/rx"
 
 const Input = ({ values, value, setData, placeholder, name, type, label }) => {
+
+  const [toggle, setToggle] = useState(false)
+  const [password, setPassword] = useState("password")
+
+  const handlePassword = (state) => {
+    if (state) {
+      setPassword("text")
+    } else {
+      setPassword ("password")
+    }
+    setToggle(!toggle)
+  }
+
   return (
     <div className="flex justify-center items-start flex-col font-lexend w-full my-4">
       <p className="text-2xl mb-2">{label}</p>
+      <div className = "rounded-full border-2 bg-white border-black px-4 py-2 text-2xl w-full">
       <input
         name={name}
-        type={type}
+        type={name === "password" ? password : type}
         placeholder={placeholder}
         value={value}
         onChange={(e) =>
           setData({ ...values, [e.target.name]: e.target.value })
         }
-        className="outline-none rounded-full border-2 border-black px-4 py-2 text-2xl w-full"
+        className="outline-none w-full"
       />
+      {
+        name === "password" && toggle && <RxEyeNone onClick = {() => handlePassword(false)} />
+      }
+      {
+        name === "password" && !toggle && <RxEyeClosed onClick = {() => handlePassword(true)} />
+      }
+      </div>
+      
     </div>
   );
 };
