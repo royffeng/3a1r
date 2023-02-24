966405219f7f3659ec2c1350758d63c8b33849a5
Alamode89 <andreamante89@gmail.com>
2023-02-23 18:19:38 -0800

    add pink hover

--- components/Signup/Genre.js
+++ components/Signup/Genre.js
@@ -8,7 +8,7 @@ const Genre = ({ genre }) => {
       onClick={() => setState(!state)}
       className={`${
         state ? "bg-micdrop-pink" : "bg-white"
-      } m-0 w-fit hover:cursor-pointer whitespace-nowrap rounded-full border-2 border-black font-lexend px-4 py-2`}
+      } m-0 w-fit hover:!bg-micdrop-pink hover:cursor-pointer whitespace-nowrap rounded-full border-2 border-black font-lexend px-4 py-2`}
     >
       {genre}
     </div>
