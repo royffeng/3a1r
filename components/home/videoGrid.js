ba3f37b8962ebc45ce8de46e09a278d45ec03d32
ashley-kim22 <ashkim316@gmail.com>
2023-03-01 04:10:55 -0800

    center videos

--- components/home/videoGrid.js
+++ components/home/videoGrid.js
@@ -3,7 +3,7 @@ import Thumbnail from "../thumbnail";
 
 export const VideoGrid = ({ videos }) => {
   return (
-    <div className = "z-0">
+    <div className = "z-0 p-0">
       {videos && (
         <>
           <Text aria-label="video count" fz={16} fw={500} className = "py-2">
@@ -13,7 +13,7 @@ export const VideoGrid = ({ videos }) => {
             <Grid
               aria-label="video-thumbnail-grid"
               gutter="md"
-              sx={{ width: "100%" }}
+              className = "flex justify-center items-start w-full m-0"
             >
               {videos?.map((video, index) => (
                 <Grid.Col
@@ -23,6 +23,7 @@ export const VideoGrid = ({ videos }) => {
                   md={6}
                   lg={3}
                   key={index}
+                  className = "m-0 p-2"
                 >
                   <Thumbnail
                     id={video.id}
@@ -36,91 +37,6 @@ export const VideoGrid = ({ videos }) => {
                   />
                 </Grid.Col>
               ))}
-                 {videos?.map((video, index) => (
-                <Grid.Col
-                  aria-label="one-video-thumbnail"
-                  xs={6}
-                  sm={6}
-                  md={6}
-                  lg={3}
-                  key={index}
-                >
-                  <Thumbnail
-                    id={video.id}
-                    thumbnail={video.thumbnail}
-                    title={video.title}
-                    username={video.profiles?.username}
-                    views={video.views}
-                    avatar_url={video.profiles?.avatar_url}
-                    date={video.created_at}
-                    userid={video.profiles?.id}
-                  />
-                </Grid.Col>
-              ))}
-                 {videos?.map((video, index) => (
-                <Grid.Col
-                  aria-label="one-video-thumbnail"
-                  xs={6}
-                  sm={6}
-                  md={6}
-                  lg={3}
-                  key={index}
-                >
-                  <Thumbnail
-                    id={video.id}
-                    thumbnail={video.thumbnail}
-                    title={video.title}
-                    username={video.profiles?.username}
-                    views={video.views}
-                    avatar_url={video.profiles?.avatar_url}
-                    date={video.created_at}
-                    userid={video.profiles?.id}
-                  />
-                </Grid.Col>
-              ))}
-                 {videos?.map((video, index) => (
-                <Grid.Col
-                  aria-label="one-video-thumbnail"
-                  xs={6}
-                  sm={6}
-                  md={6}
-                  lg={3}
-                  key={index}
-                >
-                  <Thumbnail
-                    id={video.id}
-                    thumbnail={video.thumbnail}
-                    title={video.title}
-                    username={video.profiles?.username}
-                    views={video.views}
-                    avatar_url={video.profiles?.avatar_url}
-                    date={video.created_at}
-                    userid={video.profiles?.id}
-                  />
-                </Grid.Col>
-              ))}
-                 {videos?.map((video, index) => (
-                <Grid.Col
-                  aria-label="one-video-thumbnail"
-                  xs={6}
-                  sm={6}
-                  md={6}
-                  lg={3}
-                  key={index}
-                >
-                  <Thumbnail
-                    id={video.id}
-                    thumbnail={video.thumbnail}
-                    title={video.title}
-                    username={video.profiles?.username}
-                    views={video.views}
-                    avatar_url={video.profiles?.avatar_url}
-                    date={video.created_at}
-                    userid={video.profiles?.id}
-                  />
-                </Grid.Col>
-              ))}
-              
             </Grid>
           )}
         </>
