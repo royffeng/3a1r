e94d389737d323cd59f87737285944f7fcb09f39
royffeng <roy.feiyang.feng@gmail.com>
2023-03-10 15:30:34 -0800

    formatting

--- components/profile/playlist.js
+++ components/profile/playlist.js
@@ -3,25 +3,23 @@ import { AiFillHeart } from "react-icons/ai";
 
 const Playlist = ({ playlistData }) => {
   return (
-    <div className = "border-2 border-black p-4 bg-white rounded-3xl w-full h-full">
-    <Link
-      href={`/playlists/${playlistData.id}`}
-    >
-      <div>
-        <img
-          src={playlistData.thumbnail_url}
-          className="rounded-2xl aspect-square object-cover"
-          alt="playlist thumbnail image"
-        />
-        <div className="flex justify-between items-center p-2 w-full">
-          <p className="text-2xl m-0">{playlistData.name}</p>
-          <div className="flex justify-center items-center">
-            <p className="text-2xl m-0 mx-1">{playlistData.likes}</p>
-            <AiFillHeart className="text-2xl" />
+    <div className="border-2 border-black p-4 bg-white rounded-3xl w-full h-full">
+      <Link href={`/playlists/${playlistData.id}`}>
+        <div>
+          <img
+            src={playlistData.thumbnail_url}
+            className="rounded-2xl aspect-square object-cover"
+            alt="playlist thumbnail image"
+          />
+          <div className="flex justify-between items-center p-2 w-full">
+            <p className="text-2xl m-0">{playlistData.name}</p>
+            <div className="flex justify-center items-center">
+              <p className="text-2xl m-0 mx-1">{playlistData.likes}</p>
+              <AiFillHeart className="text-2xl" />
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
     </div>
   );
 };
