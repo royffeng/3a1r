9a2bebeb77e0a9c8b742d92137ce8219878daaf8
royffeng <roy.feiyang.feng@gmail.com>
2023-03-10 15:52:35 -0800

    add backend call

--- pages/playlists/[id].js
+++ pages/playlists/[id].js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const ID = () => {
   const router = useRouter();
   const { id } = router.query;
-  console.log(id);
+
+  const supabase = useSupabaseClient();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (id) {
+        let { data, error } = await supabase
+          .from("playlistHas")
+          .select(
+          `
+            id,
+            pid, 
+            sid
+          `
+          )
+          .filter("id", "eq", id);
+        if (error) {
+          console.log("Error getting playlist songs data: ", error);
+        } else {
+          console.log(data);
+        }
+      }
+    };
+
+    fetchData();
+  }, [id]);
   return <div className="pt-20">playlists</div>;
 };
 
