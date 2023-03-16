52a4190d1fbbc9aec663df6bca342c768171007e
royffeng <roy.feiyang.feng@gmail.com>
2023-03-15 23:54:31 -0700

    personal filtering

--- components/profile/playlists.js
+++ components/profile/playlists.js
@@ -13,23 +13,44 @@ const Playlists = ({ playlists, personal = true }) => {
         {personal ? "My" : "Their"} Playlists
       </p>
       <Space h={16} />
-      {
-        personal &&  <SegmentedControl setDisplay={setDisplay} playlists={playlists} personal = {false}/>
-
-      }
+      {personal && (
+        <SegmentedControl
+          setDisplay={setDisplay}
+          playlists={playlists}
+          personal={false}
+        />
+      )}
       <Space h={32} />
       {display && (
         <Grid gutter="md">
-          {personal && display.map((video, index) => (
-            <Grid.Col xs={4} sm={4} md={4} lg={3} key={index} className="hover:cursor-pointer">
-              <Playlist playlistData={video} />
-            </Grid.Col>
-          ))}
-          {!personal && display.filter((playlist) => playlist.public === true).map((video, index) => (
-            <Grid.Col xs={4} sm={4} md={4} lg={3} key={index} className="hover:cursor-pointer">
-              <Playlist playlistData={video} />
-            </Grid.Col>
-          ))}
+          {personal &&
+            display.map((video, index) => (
+              <Grid.Col
+                xs={4}
+                sm={4}
+                md={4}
+                lg={3}
+                key={index}
+                className="hover:cursor-pointer"
+              >
+                <Playlist playlistData={video} />
+              </Grid.Col>
+            ))}
+          {!personal &&
+            display
+              .filter((playlist) => playlist.public === true)
+              .map((video, index) => (
+                <Grid.Col
+                  xs={4}
+                  sm={4}
+                  md={4}
+                  lg={3}
+                  key={index}
+                  className="hover:cursor-pointer"
+                >
+                  <Playlist playlistData={video} />
+                </Grid.Col>
+              ))}
         </Grid>
       )}
       <Space h={32} />
