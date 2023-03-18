07440c41a2978cf2286206ad9811fc219b74fe3e
Alamode89 <andreamante89@gmail.com>
2023-03-17 20:51:48 -0700

    New playlist button

--- components/profile/playlists.js
+++ components/profile/playlists.js
@@ -18,20 +18,21 @@ const Playlists = ({ playlists, personal = true }) => {
       <SegmentedControl setDisplay={setDisplay} playlists={playlists} />
       <Space h={32} />
       {display && (
-
         <Grid gutter="md">
-           <Grid.Col xs={4} sm={4} md={4} lg={3} key={-1}>
-              <div className="border-2 border-black p-4 bg-white rounded-3xl w-full h-full cursor-pointer">
-                <Link href="https://www.youtube.com/watch?v=4vbDFu0PUew">
-                  <div>
-                    <FaPlus className="text-9xl items-center"/>
-                    <div className="flex justify-between items-center p-2 w-full">
-                      <p className="text-2xl m-0">Create New Playlist</p>
-                    </div>
+          <Grid.Col xs={4} sm={4} md={4} lg={3} key={-1}>
+            <div className="border-2 border-black p-4 bg-white rounded-3xl w-full h-full cursor-pointer flex justify-center items-center">
+              <Link href="https://www.youtube.com/watch?v=4vbDFu0PUew">
+                <div className = "w-full h-full">
+                  <div className="flex justify-center items-center flex-col bg-micdrop-beige rounded-2xl w-full h-5/6 border-black border-2">
+                    <FaPlus className="text-9xl flex content-center" />
                   </div>
-                </Link>
-              </div>
-            </Grid.Col>
+                  <div className="flex justify-between items-center p-2 w-full">
+                    <p className="text-2xl mb-0 text-center w-full">Create New Playlist</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </Grid.Col>
           {display?.map((video, index) => (
             <Grid.Col xs={4} sm={4} md={4} lg={3} key={index}>
               <Playlist playlistData={video} />
--- pages/playlists/[id].js
+++ pages/playlists/[id].js
@@ -106,62 +106,62 @@ const ID = ({ searchContext }) => {
 
   return (
     <>
-    <Navbar searchContext={searchContext}/>
-    <div className="pt-20 px-4">
-      <Row className="flex justify-center items-center">
-        {playlist && (
-          <>
-            {" "}
-            <Col xl={2} className="flex justify-center items-center">
-              <img src={playlist.thumbnail_url} className="rounded-xl" />
-            </Col>
-            <Col xl={10}>
-              <div className="flex flex-col justify-center items-start">
-                <div className="font-bold text-6xl my-2">{playlist.name}</div>
-                <div className="font-semibold text-3xl my-4">
-                  public playlist
-                </div>
-                <div className="mb-2">{videos.length} videos</div>
-              </div>
-              {profile && (
-                <div className="flex justify-between items-center">
-                  <div className="flex items-center">
-                    <img
-                      src={profile.avatar_url}
-                      className="rounded-full w-10 aspect-square object-cover mr-2"
-                    />
-                    <p className= "mb-0">{profile.username}</p>
+      <Navbar searchContext={searchContext} />
+      <div className="pt-20 px-4">
+        <Row className="flex justify-center items-center">
+          {playlist && (
+            <>
+              {" "}
+              <Col xl={2} className="flex justify-center items-center">
+                <img src={playlist.thumbnail_url} className="rounded-xl" />
+              </Col>
+              <Col xl={10}>
+                <div className="flex flex-col justify-center items-start">
+                  <div className="font-bold text-6xl my-2">{playlist.name}</div>
+                  <div className="font-semibold text-3xl my-4">
+                    public playlist
                   </div>
-                  <div className = "flex items-center">
-                    <p className="text-2xl mb-0 mx-1">{playlist.likes}</p>
-                    <AiFillHeart className="text-2xl" />
-                  </div>
+                  <div className="mb-2">{videos.length} videos</div>
                 </div>
-              )}
-            </Col>
-          </>
-        )}
-      </Row>
-      <div className="w-full h-1 bg-black my-4" />
-      <Row>
-        {videos.length > 0 &&
-          videos.map((video, index) => (
-            <Col xl={3} key={index}>
-              <Thumbnail
-                id={video.id}
-                username={video.profiles?.username}
-                title={video.title}
-                views={video.views}
-                thumbnail={video.thumbnail}
-                avatar_url={video.profiles?.avatar_url}
-                date={video.created_at}
-                noDate={true}
-                userid={video.profiles?.id}
-              />
-            </Col>
-          ))}
-      </Row>
-    </div>
+                {profile && (
+                  <div className="flex justify-between items-center">
+                    <div className="flex items-center">
+                      <img
+                        src={profile.avatar_url}
+                        className="rounded-full w-10 aspect-square object-cover mr-2"
+                      />
+                      <p className="mb-0">{profile.username}</p>
+                    </div>
+                    <div className="flex items-center">
+                      <p className="text-2xl mb-0 mx-1">{playlist.likes}</p>
+                      <AiFillHeart className="text-2xl" />
+                    </div>
+                  </div>
+                )}
+              </Col>
+            </>
+          )}
+        </Row>
+        <div className="w-full h-1 bg-black my-4" />
+        <Row>
+          {videos.length > 0 &&
+            videos.map((video, index) => (
+              <Col xl={3} key={index}>
+                <Thumbnail
+                  id={video.id}
+                  username={video.profiles?.username}
+                  title={video.title}
+                  views={video.views}
+                  thumbnail={video.thumbnail}
+                  avatar_url={video.profiles?.avatar_url}
+                  date={video.created_at}
+                  noDate={true}
+                  userid={video.profiles?.id}
+                />
+              </Col>
+            ))}
+        </Row>
+      </div>
     </>
   );
 };
