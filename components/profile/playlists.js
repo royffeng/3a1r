aad0b19ff017c78d228cef877e253a1a7db580c9
Alamode89 <andreamante89@gmail.com>
2023-03-17 20:20:20 -0700

    Create new playlist has link

--- components/profile/playlists.js
+++ components/profile/playlists.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Space, Grid } from "@mantine/core";
 import { FaPlus } from "react-icons/fa";
+import Link from "next/link";
 import SegmentedControl from "./SegmentedControl";
 import Playlist from "./playlist";
 
@@ -20,8 +21,15 @@ const Playlists = ({ playlists, personal = true }) => {
 
         <Grid gutter="md">
            <Grid.Col xs={4} sm={4} md={4} lg={3} key={-1}>
-              <div className="border-2 border-black p-4 bg-white rounded-3xl w-full h-full">
-                <FaPlus className="text-9xl rounded-2xl aspect-square object-cover"/>
+              <div className="border-2 border-black p-4 bg-white rounded-3xl w-full h-full cursor-pointer">
+                <Link href="https://www.youtube.com/watch?v=4vbDFu0PUew">
+                  <div>
+                    <FaPlus className="text-9xl items-center"/>
+                    <div className="flex justify-between items-center p-2 w-full">
+                      <p className="text-2xl m-0">Create New Playlist</p>
+                    </div>
+                  </div>
+                </Link>
               </div>
             </Grid.Col>
           {display?.map((video, index) => (
