30c8ad84f0dde8741c8c210e37f9b9d975af8e9f
Alamode89 <andreamante89@gmail.com>
2023-03-20 01:58:11 -0700

    save

--- pages/createplaylist.js
+++ pages/createplaylist.js
@@ -3,11 +3,12 @@ import { useRouter } from "next/router";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Thumbnail from "../components/thumbnail";
 import { Row, Col } from "react-bootstrap";
-import { AiFillHeart } from "react-icons/ai";
+import { FaCamera } from "react-icons/fa"
 import Navbar from "../components/navbar";
 
 
 const createplaylist = ({ searchContext }) => {
+    
     const router = useRouter();
   const { id } = router.query;
 
@@ -106,45 +107,21 @@ const createplaylist = ({ searchContext }) => {
   }, [id]);
 
   return (
-    <div>
+    <>
       <Navbar searchContext={searchContext} />
       <div className="pt-20 px-4">
         <Row className="flex justify-center items-center">
-          {playlist && (
-            <>
-              {" "}
-              <Col xl={2} className="flex justify-center items-center">
-                <img src={playlist.thumbnail_url} className="rounded-xl" />
-              </Col>
-              <Col xl={10}>
-                <div className="flex flex-col justify-center items-start">
-                  <div className="font-bold text-6xl my-2">{playlist.name}</div>
-                  <div className="font-semibold text-3xl my-4">
-                    public playlist
-                  </div>
-                  <div className="mb-2">{videos.length} videos</div>
+            <Col xl={2} className="flex justify-center items-center">
+            <div className="w-full h-full">
+                    <div className="flex justify-center items-center flex-col bg-micdrop-beige rounded-2xl w-full h-5/6 border-black border-2">
+                <FaCamera className="text-8xl flex content-center" />
                 </div>
-                {profile && (
-                  <div className="flex justify-between items-center">
-                    <div className="flex items-center">
-                      <img
-                        src={profile.avatar_url}
-                        className="rounded-full w-10 aspect-square object-cover mr-2"
-                      />
-                      <p className="mb-0">{profile.username}</p>
-                    </div>
-                    <div className="flex items-center mr-3">
-                      <p className="text-2xl mb-0 mx-1">{playlist.likes}</p>
-                      <AiFillHeart className="text-2xl" />
-                    </div>
-                  </div>
-                )}
-              </Col>
-            </>
-          )}
+            </div>
+            </Col>
+
         </Row>
-        </div>
-    </div>
+      </div>
+    </>
   );
 };
 
